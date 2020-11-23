2e2cc24d0438494e7a662de5f2bfda2ad4d69f55
Derick <derick290801@gmail.com>
2020-11-23 18:02:31 -0400

    Log In page design completed

---
+++ MVVMFirstProject/MVVMFirstProject/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MVVMFirstProject.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVMFirstProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new LogInPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
