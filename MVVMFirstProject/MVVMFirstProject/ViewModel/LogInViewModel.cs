3c7fd58a1aa9babbccd89671829d33d750ed3488
Derick <derick290801@gmail.com>
2020-11-26 19:06:31 -0400

    Traveling through the app is posible with INotifyPropertyChanged

--- MVVMFirstProject/MVVMFirstProject/ViewModel/LogInViewModel.cs
+++ MVVMFirstProject/MVVMFirstProject/ViewModel/LogInViewModel.cs
@@ -9,13 +9,64 @@ using Xamarin.Forms;
 
 namespace MVVMFirstProject.ViewModel
 {
-    class LogInViewModel
+    class LogInViewModel : INotifyPropertyChanged
     {
-        public User user1 = new User();
-        public String email { get; set; }
-        public String password { get; set; }
+        private String varName, varEmail, varPassword, varConfirmPassword;
+        public String Name
+        {
+            get
+            {
+                return varName;
+            }
+            set
+            {
+                varName = value;
+                PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+        public String Email
+        {
+            get
+            {
+                return varEmail;
+            }
+            set
+            {
+                varEmail = value;
+                PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs(nameof(Email)));
+            }
+        }
+        public String Password
+        {
+            get
+            {
+                return varPassword;
+            }
+            set
+            {
+                varPassword = value;
+                PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs(nameof(Password)));
+            }
+        }
+        public String ConfirmPassword
+        {
+            get
+            {
+                return varConfirmPassword;
+            }
+            set
+            {
+                varConfirmPassword = value;
+                PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs(nameof(ConfirmPassword)));
+            }
+        }
 
-        /*public LogInViewModel user = new LogInViewModel();*/
+        public event PropertyChangedEventHandler PropertyChanged;
+        
         public ICommand LogInCommand => new Command(LogButtonClicked);
         public ICommand RegisterCommand => new Command(RegisterButtonClicked);
 
@@ -23,7 +74,7 @@ namespace MVVMFirstProject.ViewModel
         async private void LogButtonClicked()
         {
             //Check if there's any empty entry
-            if(string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            if(string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
             {
                 await App.Current.MainPage.DisplayAlert("No se puede completar la acción",
                     $"Debe de completar todos los campos", "Ok");
